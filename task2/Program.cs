cd394fa73d873858b9e4a7a6efdfdb95546a19ef
Alina Yagnaeva <as.yagnaeva97@gmail.com>
2023-11-06 19:21:44 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt (string message)
+{
+    Console.WriteLine(message);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int Summ (int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result += number % 10;
+        number = number / 10;
+    }
+    return result;
+}
+
+int number = Prompt ("Введите число: ");
+Console.WriteLine ($"Сумма числе в цифре {number}  равна {Summ(number)}");
